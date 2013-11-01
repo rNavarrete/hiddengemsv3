05eaa8ac01df92b169feb431233ac15a4991f8d3
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-10-31 19:22:30 -0700

    added validations to videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,11 @@ class Video < ActiveRecord::Base
  acts_as_voteable
  belongs_to :user
 
+
+ validates :artist, length: { maximum: 31, too_long: "%{count} characters is the maximum allowed" } 
+ validates :song, length: { maximum: 31 }
+
+
   def self.text_search(query)
     if query.present?
  	    search(query)
